8cac94c056f0db6d14cd69976f983e8dc6e8cb0b
drisspg <drisspguessous@gmail.com>
2026-06-03 02:52:58 +0000

    Restrict MX scaled wrapper tests to CUDA

--- test/test_scaled_matmul_cuda.py
+++ test/test_scaled_matmul_cuda.py
@@ -2159,6 +2159,7 @@ class TestFP8Matmul(TestCase):
             if sqnr.item() <= approx_match_sqnr_target:
                 raise AssertionError(f"sqnr {sqnr.item()} should be > {approx_match_sqnr_target}")
 
+    @onlyCUDA
     @unittest.skipIf(not PLATFORM_SUPPORTS_MX_GEMM or IS_WINDOWS, mx_skip_msg)
     def test_functional_scaled_mm_single_value_expansion(self, device) -> None:
         M, N, K = 128, 128, 128
@@ -2194,6 +2195,7 @@ class TestFP8Matmul(TestCase):
         )
         self.assertEqual(actual, expected)
 
+    @onlyCUDA
     @unittest.skipIf(not PLATFORM_SUPPORTS_MX_GEMM or IS_WINDOWS, mx_skip_msg)
     def test_functional_scaled_mm_rejects_mismatched_scale_lists(self, device) -> None:
         M, N, K = 128, 128, 128
